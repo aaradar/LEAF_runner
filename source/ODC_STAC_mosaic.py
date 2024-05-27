8f744282918649b0aeaf631caa2a639126b1268d
fqqlsun <fqqslx@yahoo.com>
2024-05-27 09:19:11 -0400

    May 26, 2024

--- source/ODC_STAC_mosaic.py
+++ source/ODC_STAC_mosaic.py
@@ -8,6 +8,23 @@ from dask.diagnostics import ProgressBar
 #from odc.geo.geobox import GeoBox
 #from rasterio.enums import Resampling
 
+UNKNOWN_sensor = 0
+LS5_sensor     = 5
+LS7_sensor     = 7
+LS8_sensor     = 8
+LS9_sensor     = 9
+LS_sensor      = 19
+MAX_LS_CODE    = 20
+S2A_sensor     = 21
+S2B_sensor     = 22
+S1B_sensor     = 41
+S1B_sensor     = 42
+
+
+MOD_sensor     = 50     # MODIS sensor
+HLS_sensor     = 100    # Harmonized Landsat and Sentinel-2
+
+
 
 #==================================================================================================
 # define a spatial region around Ottawa
@@ -24,6 +41,85 @@ ottawa_region = {
        ]
     ]
 }
+
+
+tile55_922 = {
+    'type': 'Polygon',
+    'coordinates': [
+       [
+         [-77.6221, 47.5314], 
+         [-73.8758, 46.7329],
+         [-75.0742, 44.2113], 
+         [-78.6303, 44.9569],
+         [-77.6221, 47.5314]
+       ]
+    ]
+}
+
+
+
+
+
+#############################################################################################################
+# Description: This function returns a boundary box [min(longs), min(lats), max(longs), max(lats)] based on 
+#              a given geographic region defined by Lats/Longs.
+#
+# Revision history:  2024-May-27  Lixin Sun  Initial creation
+# 
+#############################################################################################################
+def get_region_bbox(inRegion):
+  coords = inRegion['coordinates'][0]
+  nCoords = len(coords)
+   
+  longs = []
+  lats  = []
+
+  for i in range(nCoords):
+    longs.append(coords[i][0]) 
+    lats.append(coords[i][1])
+
+  return [min(longs), min(lats), max(longs), max(lats)]
+
+
+
+
+#############################################################################################################
+# Description: This function divides the bbox associated with a given geographic region (defined by 
+#              Lats/Longs) into a number of sub-bboxes
+#
+# Revision history:  2024-May-27  Lixin Sun  Initial creation
+# 
+#############################################################################################################
+def divide_region(inRegion, nDivides):
+  if nDivides <= 0:
+    nDivides = 2
+
+  # Obtain the bbox of the given geographic region 
+  bbox = get_region_bbox(inRegion)
+  left_lon = bbox[0]
+  btom_Lat = bbox[1]
+
+  lon_delta = (bbox[2] - left_lon)/nDivides
+  lat_delta = (bbox[3] - btom_Lat)/nDivides
+
+  sub_regions = []  
+  for i in range(nDivides):    
+    for j in range(nDivides):
+      sub_region = []
+      BL_lon = left_lon+i*lon_delta
+      BL_lat = btom_Lat+j*lat_delta
+
+      sub_region.append([BL_lon, BL_lat])
+      sub_region.append([BL_lon+lon_delta, BL_lat])
+      sub_region.append([BL_lon+lon_delta, BL_lat+lat_delta])
+      sub_region.append([BL_lon, BL_lat+lat_delta])
+      sub_region.append([BL_lon, BL_lat])
+  
+      sub_regions.append(sub_region)
+  
+  return sub_regions
+
+
 
 
 
@@ -49,8 +145,8 @@ def get_resolution(xr_img_coll):
   # Inspect the first item's metadata
   first_item = xr_img_coll[0]
   #print(first_item.to_dict())
-  #bands = first_item.assets.keys()
-  #print("Available bands in the first item:", bands)
+  bands = first_item.assets.keys()
+  print("Available bands in the first item:", bands)
 
   # Look for the 'proj:transform' or 'gsd' properties to infer resolution  
   if 'proj:transform' in first_item.properties:
@@ -78,14 +174,9 @@ def get_resolution(xr_img_coll):
 # Revision history:  2024-May-24  Lixin Sun  Initial creation
 # 
 #############################################################################################################
-def get_STAC_IC(CatalogName, CollName, Region, ProjStr, StartStr, EndStr):
-  # use publically available stac link such as
-  catalog = pystac_client.Client.open(str(CatalogName)) 
-  #catalog = pystac_client.Client.open("https://earth-search.aws.element84.com/v1") 
-
-  # define a temporal window
-  timeframe = str(StartStr) + '/' + str(EndStr)
-
+def get_query_conditions(ssr_code, StartStr, EndStr):
+  query_conds = {}
+  
   #==================================================================================================
   # Create a filter for the search based on metadata. The filtering params will depend upon the 
   # image collection we are using. e.g. in case of Sentine 2 L2A, we can use params such as: 
@@ -99,15 +190,91 @@ def get_STAC_IC(CatalogName, CollName, Region, ProjStr, StartStr, EndStr):
   # s2:snow_ice_percentage, etc.
   # 
   #==================================================================================================
-  filters = {"s2:cloud_shadow_percentage": {"lt": 0.9} }
+  query_conds['filters'] = {"s2:cloud_shadow_percentage": {"lt": 0.9} }
+
+  if ssr_code > MAX_LS_CODE & ssr_code < MOD_sensor:
+    query_conds['catalog']    = "https://earth-search.aws.element84.com/v1"
+    query_conds['collection'] = "sentinel-2-l2a"
+    query_conds['timeframe']  = str(StartStr) + '/' + str(EndStr)
+    query_conds['bands']      = ['blue', 'green', 'red', 'nir08', 'swir16', 'swir22', 'scl']
+
+  return query_conds
+
+
+
+
+
+#############################################################################################################
+# Description: This function returns a base image.
+#
+# Revision history:  2024-May-24  Lixin Sun  Initial creation
+# 
+#############################################################################################################
+def get_base_Image(S2A_sensor, Region, ProjStr, Scale, StartStr, EndStr):
+  # get all query conditions 
+  query_conds = get_query_conditions(S2A_sensor, StartStr, EndStr)
+
+  # use publically available stac link such as
+  catalog = pystac_client.Client.open(str(query_conds['catalog'])) 
 
   #==================================================================================================
   # Search and filter a image collection
   #==================================================================================================  
-  search_IC = catalog.search(collections = [str(CollName)], 
+  search_IC = catalog.search(collections = [str(query_conds['collection'])], 
                              intersects  = Region,                            
-                             datetime    = timeframe, 
-                             query       = filters,
+                             datetime    = str(query_conds['timeframe']), 
+                             query       = query_conds['filters'],
+                             limit       = 1)
+  
+  items = list(search_IC.items())
+  print(f"Found: {len(items):d} datasets")
+  
+  #==================================================================================================
+  # define a geobox for my region
+  #==================================================================================================
+  # lazily combine items
+  mybbox = get_region_bbox(Region)
+  print('<get_STAC_ImColl> The bbox of the given region = ', mybbox)
+
+  ds_xr = odc.stac.load([items[0], items[1]],
+                        bands  = query_conds['bands'],
+                        chunks = {'x': 1000, 'y': 1000},
+                        crs    = ProjStr, 
+                        bbox   = mybbox,
+                        resolution = Scale)
+
+  # actually load it
+  with ProgressBar():
+    ds_xr.load()
+
+  return ds_xr.isel(time=0)
+
+
+
+
+
+#############################################################################################################
+# Description: This function returns a collection of images from a specified catalog and collection based on
+#              given spatial region, timeframe and filtering criteria. The returned image collection will be 
+#              stored in a xarray.Dataset structure.
+#
+# Revision history:  2024-May-24  Lixin Sun  Initial creation
+# 
+#############################################################################################################
+def get_STAC_ImColl(S2A_sensor, Region, ProjStr, Scale, StartStr, EndStr, GroupBy=True):
+  # get all query conditions 
+  query_conds = get_query_conditions(S2A_sensor, StartStr, EndStr)
+
+  # use publically available stac link such as
+  catalog = pystac_client.Client.open(str(query_conds['catalog'])) 
+
+  #==================================================================================================
+  # Search and filter a image collection
+  #==================================================================================================  
+  search_IC = catalog.search(collections = [str(query_conds['collection'])], 
+                             intersects  = Region,                            
+                             datetime    = str(query_conds['timeframe']), 
+                             query       = query_conds['filters'],
                              limit       = 200)
   
   items = list(search_IC.items())
@@ -115,8 +282,6 @@ def get_STAC_IC(CatalogName, CollName, Region, ProjStr, StartStr, EndStr):
   
   #first_item = items[0].to_dict()
   #print(first_item)
-  #resolu = get_resolution(items)
-
   '''
   # Spit out data as GeoJSON dictionary  
   print(search_IC.item_collection_as_dict())
@@ -134,15 +299,18 @@ def get_STAC_IC(CatalogName, CollName, Region, ProjStr, StartStr, EndStr):
   #geobox = GeoBox.from_bbox(ottawa_region, crs=f"epsg:{epsg}", resolution=dx)
 
   # lazily combine items
-  band_names = ['blue', 'green', 'red', 'nir08', 'swir16', 'swir22', 'scl']
+  mybbox = get_region_bbox(Region)
+  print('<get_STAC_ImColl> The bbox of the given region = ', mybbox)
 
   ds_xr = odc.stac.load(search_IC.items(),
-                        bands  = band_names,
-                        #groupby='solar_day',
-                        chunks = {'time': 5, 'x': 1000, 'y': 1000},
-                        crs = ProjStr, 
-                        #geobox = ottawa_region,
-                        resolution = 100)
+                        bands  = query_conds['bands'],
+                        groupby='solar_day',  #For lower latitude scenses, save a lot memories
+                        chunks = {'x': 1000, 'y': 1000},
+                        crs    = ProjStr, 
+                        bbox   = mybbox,
+                        resolution = Scale)
+
+  return ds_xr  
 
   # actually load it
   with ProgressBar():
@@ -189,3 +357,4 @@ rgb_median = (
 )
 (rgb_median / rgb_median.max() * 2).plot.imshow(rgb="band", figsize=(10, 8))
 '''
+
